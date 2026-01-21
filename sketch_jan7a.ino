eab3388f510c96ee776db5907d66d2b624ed9d05
Delosle <ldenglos31@gmail.com>
2026-01-21 15:11:10 +0100

    ajout code arduino

---
+++ sketch_jan7a.ino
@@ -0,0 +1,64 @@
+// Acquisition avec référence interne 1.1V
+// Résolution: 1.1V / 1023 = 1.075 mV (au lieu de 4.9 mV)
+// Idéal pour mesurer ~0.52V
+
+const int analogPin = A0;
+const int bufferSize = 512;
+
+volatile uint16_t buffer[bufferSize];
+volatile uint16_t bufferIndex = 0;
+volatile bool bufferReady = false;
+
+void setup() {
+  Serial.begin(2000000);
+  
+  // IMPORTANT: Référence interne 1.1V pour meilleure résolution
+  analogReference(INTERNAL);  // 1.1V au lieu de 5V
+  
+  // Attendre stabilisation de la référence (critique !)
+  delay(500);
+  
+  // Faire plusieurs lectures pour stabiliser
+  for (int i = 0; i < 10; i++) {
+    analogRead(analogPin);
+    delay(10);
+  }
+  
+  // Configuration ADC rapide
+  ADCSRA &= ~0x07;
+  ADCSRA |= 0x05;            // Prescaler = 32
+  ADCSRA |= (1 << ADATE);    // Auto-trigger
+  ADCSRB = 0x00;             // Free running
+  ADCSRA |= (1 << ADIE);     // Interrupt
+  ADCSRA |= (1 << ADEN);
+  ADCSRA |= (1 << ADSC);
+  
+  delay(100);
+  
+  // En-tête
+  Serial.write(0xFF);
+  Serial.write(0xAA);
+  Serial.write(0x55);
+  Serial.write(0xFF);
+}
+
+ISR(ADC_vect) {
+  if (!bufferReady) {
+    buffer[bufferIndex++] = ADC;
+    if (bufferIndex >= bufferSize) {
+      bufferIndex = 0;
+      bufferReady = true;
+    }
+  }
+}
+
+void loop() {
+  if (bufferReady) {
+    // Transmission binaire
+    for (int i = 0; i < bufferSize; i++) {
+      Serial.write((uint8_t)(buffer[i] & 0xFF));
+      Serial.write((uint8_t)((buffer[i] >> 8) & 0xFF));
+    }
+    bufferReady = false;
+  }
+}
